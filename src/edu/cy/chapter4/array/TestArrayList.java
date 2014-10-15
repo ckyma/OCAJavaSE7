f65b9060ccf551105a3628fe2d03ae9d9288c95e
Yu Chen <yu.chen@phinarys.com>
2014-10-15 20:23:39 +0800

    Fixed name

--- src/edu/cy/chapter4/array/TestArrayList.java
+++ src/edu/cy/chapter4/array/TestArrayList.java
@@ -3,12 +3,12 @@ package edu.cy.chapter4.array;
 import java.util.ArrayList;
 
 /**
- * Created by phinary0 on 10/15/14.
+ * Created by CY on 10/15/14.
  */
 public class TestArrayList {
     public static void main(String[] args) {
         ArrayList<String> myArrayList1 = new ArrayList<String>();
         ArrayList<String> myArrayList2 = new ArrayList<>();
-        
+
     }
 }
